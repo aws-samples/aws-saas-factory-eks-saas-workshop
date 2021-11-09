ff2a7e8fde6de9841819adaa451b8cef8f06013f
Toby Buckley <tobuck@amazon.com>
2021-11-08 17:56:31 -0800

    Removed extraneous bash commands

--- scripts/deploy-core-services.sh
+++ scripts/deploy-core-services.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 source ./scripts/deploy-admin.sh
-TENANTPATH=app envsubst < ./client/web/admin/k8s/template.txt > ./client/web/admin/k8s/template.yaml
+envsubst < ./client/web/admin/k8s/template.txt > ./client/web/admin/k8s/template.yaml
 kubectl apply -f ./client/web/admin/k8s/template.yaml
 
 source ./scripts/deploy-tenant-registration.sh
-TENANTPATH=app envsubst < ./services/shared/apps/tenant-registration/k8s/template.txt > ./services/shared/apps/tenant-registration/k8s/template.yaml
+envsubst < ./services/shared/apps/tenant-registration/k8s/template.txt > ./services/shared/apps/tenant-registration/k8s/template.yaml
 kubectl apply -f ./services/shared/apps/tenant-registration/k8s/template.yaml
 
 source ./scripts/deploy-tenant-management.sh
-TENANTPATH=app envsubst < ./services/shared/apps/tenant-management/k8s/template.txt > ./services/shared/apps/tenant-management/k8s/template.yaml
+envsubst < ./services/shared/apps/tenant-management/k8s/template.txt > ./services/shared/apps/tenant-management/k8s/template.yaml
 kubectl apply -f ./services/shared/apps/tenant-management/k8s/template.yaml
