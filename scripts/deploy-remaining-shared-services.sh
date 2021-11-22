cd2e69819fe23187515ad369b3e9918bb140c6aa
Toby Buckley <tobuck@amazon.com>
2021-11-22 09:43:36 -0800

    Removed users feature from both apps Added script to "deploy remaining services" Updated deploy-core-services to include user-management

--- scripts/deploy-remaining-shared-services.sh
+++ scripts/deploy-remaining-shared-services.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-source ./scripts/deploy-admin.sh
-envsubst < ./client/web/admin/k8s/template.txt > ./client/web/admin/k8s/template.yaml
-kubectl apply -f ./client/web/admin/k8s/template.yaml
-
-source ./scripts/deploy-tenant-registration.sh
-envsubst < ./services/shared/apps/tenant-registration/k8s/template.txt > ./services/shared/apps/tenant-registration/k8s/template.yaml
-kubectl apply -f ./services/shared/apps/tenant-registration/k8s/template.yaml
-
 source ./scripts/deploy-tenant-management.sh
 envsubst < ./services/shared/apps/tenant-management/k8s/template.txt > ./services/shared/apps/tenant-management/k8s/template.yaml
 kubectl apply -f ./services/shared/apps/tenant-management/k8s/template.yaml
