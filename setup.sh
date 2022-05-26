1aaa5b22770ea3899508cda34d2ba3ab6fc9eeab
Ivor Scott Cummings <ivor@devpie.io>
2022-05-26 13:29:37 +0200

    fix: pin down kubectl version
    fixes aws eks update-kubeconfig invalid apiVersion https://github.com/aws/aws-cli/issues/6920

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 CWD=$(pwd)
 
 echo "Installing kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO https://dl.k8s.io/release/v1.23.6/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 export NVM_DIR=$HOME/.nvm;
