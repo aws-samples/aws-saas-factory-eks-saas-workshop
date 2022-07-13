f32d5ab6bda30d6741eae90185010cda52cc620d
Humberto Somensi <somensi@amazon.com>
2022-07-13 23:17:41 +0100

    removed redundant catch blocks

--- services/apps/application/product/src/products/products.service.ts
+++ services/apps/application/product/src/products/products.service.ts
@@ -27,119 +27,71 @@ export class ProductsService {
       product_id: uuid(),
       tenant_id: tenantId,
     };
-    console.log('Creating product:', newProduct);
-
-    try {
-      const client = await this.fetchClient(tenantId);
-      const cmd = new PutCommand({
-        Item: newProduct,
-        TableName: this.tableName,
-      });
-      client.send(cmd);
-    } catch (error) {
-      console.error(error);
-      throw new HttpException(
-        {
-          status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'Something went wrong',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    console.log('Creating product:', newProduct);    
+    const client = await this.fetchClient(tenantId);
+    const cmd = new PutCommand({
+      Item: newProduct,
+      TableName: this.tableName,
+    });
+    client.send(cmd);   
   }
 
   async findAll(tenantId: string) {
     console.log('Getting All Products for Tenant:', tenantId);
-    try {
-      const client = await this.fetchClient(tenantId);
-      const cmd = new QueryCommand({
-        TableName: this.tableName,
-        KeyConditionExpression: 'tenant_id=:t_id',
-        ExpressionAttributeValues: {
-          ':t_id': tenantId,
-        },
-      });
-
-      const response = await client.send(cmd);
-      return JSON.stringify(response.Items);
-    } catch (error) {
-      console.error(error);
-      throw new HttpException(
-        {
-          status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'Something went wrong',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    const client = await this.fetchClient(tenantId);
+    const cmd = new QueryCommand({
+      TableName: this.tableName,
+      KeyConditionExpression: 'tenant_id=:t_id',
+      ExpressionAttributeValues: {
+        ':t_id': tenantId,
+      },
+    });
+    const response = await client.send(cmd);
+    return JSON.stringify(response.Items);    
   }
 
-  async findOne(id: string, tenantId: string) {
-    try {
-      console.log('Getting Product: ', id);
-      const client = await this.fetchClient(tenantId);
-      const cmd = new QueryCommand({
-        TableName: this.tableName,
-        KeyConditionExpression: 'tenant_id=:t_id AND product_id=:p_id',
-        ExpressionAttributeValues: {
-          ':t_id': tenantId,
-          ':p_id': id,
-        },
-      });
-      const response = await client.send(cmd);
-      return JSON.stringify(response.Items && response.Items[0]);
-    } catch (error) {
-      console.error(error);
-      throw new HttpException(
-        {
-          status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'Something went wrong',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  async findOne(id: string, tenantId: string) {    
+    console.log('Getting Product: ', id);
+    const client = await this.fetchClient(tenantId);
+    const cmd = new QueryCommand({
+      TableName: this.tableName,
+      KeyConditionExpression: 'tenant_id=:t_id AND product_id=:p_id',
+      ExpressionAttributeValues: {
+        ':t_id': tenantId,
+        ':p_id': id,
+      },
+    });
+    const response = await client.send(cmd);
+    return JSON.stringify(response.Items && response.Items[0]);    
   }
 
   async update(
     id: string,
     tenantId: string,
-    updateProductDto: UpdateProductDto,
-  ) {
-    try {
-      console.log('Updating Product: ', id);
-      const client = await this.fetchClient(tenantId);
-      const cmd = new UpdateCommand({
-        TableName: this.tableName,
-        Key: {
-          tenant_id: tenantId,
-          product_id: id,
-        },
-        UpdateExpression: 'set #name = :n, #price = :p, #description = :d',
-        ExpressionAttributeValues: {
-          ':n': updateProductDto.name,
-          ':p': updateProductDto.price,
-          ':d': updateProductDto.description,
-        },
-        ExpressionAttributeNames: {
-          '#name': 'name',
-          '#price': 'price',
-          '#description': 'description',
-        },
-      });
-
-      const response = await client.send(cmd);
-      console.log('Update Response:', response);
-      return JSON.stringify(updateProductDto);
-    } catch (error) {
-      console.error(error);
-      throw new HttpException(
-        {
-          status: HttpStatus.INTERNAL_SERVER_ERROR,
-          error: 'Something went wrong',
-        },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    updateProductDto: UpdateProductDto) {    
+    console.log('Updating Product: ', id);
+    const client = await this.fetchClient(tenantId);
+    const cmd = new UpdateCommand({
+      TableName: this.tableName,
+      Key: {
+        tenant_id: tenantId,
+        product_id: id,
+      },
+      UpdateExpression: 'set #name = :n, #price = :p, #description = :d',
+      ExpressionAttributeValues: {
+        ':n': updateProductDto.name,
+        ':p': updateProductDto.price,
+        ':d': updateProductDto.description,
+      },
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#price': 'price',
+        '#description': 'description',
+      },
+    });
+    const response = await client.send(cmd);
+    console.log('Update Response:', response);
+    return JSON.stringify(updateProductDto);      
   }
 
   async fetchClient(tenantId: string) {
