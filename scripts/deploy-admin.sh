16d0fb2d65641c899b123fa857b5759d1527488c
Toby Buckley <tobuck@amazon.com>
2022-07-25 14:55:52 -0700

    Fixing scripts and templates

--- scripts/deploy-admin.sh
+++ scripts/deploy-admin.sh
@@ -2,54 +2,7 @@
 
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-
 export USERPOOLID=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AdminUserPoolId'].OutputValue" --output text)
 export APPCLIENTID=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AdminAppClientId'].OutputValue" --output text)
-export ADMINAPPLICATIONECR=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AdminApplicationECR'].OutputValue" --output text)
+export ADMINAPPIMAGE='public.ecr.aws/o2b5n0j5/eks-saas-admin:latest'
 export REGION=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AWSRegion'].OutputValue" --output text)
-
-CWD=$(pwd)
-
-mkdir ./client/web/admin/src/environments
-
-cat << EoF > ./client/web/admin/src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'http://$ELBURL',
-};
-
-EoF
-cat << EoF > ./client/web/admin/src/environments/environment.ts
-export const environment = {
-  production: true,
-  apiUrl: 'http://$ELBURL',
-};
-EoF
-
-cat << EoF > ./client/web/admin/src/aws-exports.js
-const awsmobile = {
-    "aws_project_region": "$REGION",
-    "aws_cognito_region": "$REGION",
-    "aws_user_pools_id": "$USERPOOLID",
-    "aws_user_pools_web_client_id": "$APPCLIENTID",
-};
-
-
-export default awsmobile;
-EoF
-
-cd ./client/web/admin
-REGISTRY=$(echo $ADMINAPPLICATIONECR| cut -d'/' -f 1)
-
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY
-docker build -t eks-admin-app:latest .
-docker tag eks-admin-app:latest $ADMINAPPLICATIONECR:latest
-docker push $ADMINAPPLICATIONECR:latest
-
-echo "The following values will need to be plugged into the Admin Service Kubernetes manifest before deployment."
-echo "**PLEASE DO NOT CLOSE THIS WINDOW**"
-echo "";
-echo "ADMIN_APPLICATION_ECR:" $ADMINAPPLICATIONECR
-echo "ELB_URL:" $ELBURL
-
-cd $CWD
--- scripts/deploy-tenant-management.sh
+++ scripts/deploy-tenant-management.sh
@@ -2,27 +2,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-export TENANTMANAGEMENTECR=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='TenantMangementECR'].OutputValue" --output text)
 export REGION=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AWSRegion'].OutputValue" --output text)
 export USERPOOLID=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AdminUserPoolId'].OutputValue" --output text)
 export APPCLIENTID=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AdminAppClientId'].OutputValue" --output text)
 export TENANT_TABLE_NAME=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='TenantTable'].OutputValue" --output text)
 export AUTH_INFO_TABLE_NAME=$(aws cloudformation describe-stacks --stack-name RootStack --query "Stacks[0].Outputs[?OutputKey=='AuthInfoTable'].OutputValue" --output text)
-# ELBURL=$(aws cloudformation describe-stacks --stack-name EksStack --query "Stacks[0].Outputs[?OutputKey=='ELBURL'].OutputValue" --output text)
-CWD=$(pwd)
-cd ./services/shared
-REGISTRY=$(echo $TENANTMANAGEMENTECR| cut -d'/' -f 1)
-TENANTREGCONTAINERNAME=$(echo $TENANTMANAGEMENTECR| cut -d'/' -f 2)
-
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY
-docker build -t tenant-mgmt-svc:latest -f Dockerfile.tenant-management .
-docker tag tenant-mgmt-svc:latest $TENANTMANAGEMENTECR:latest
-docker push $TENANTMANAGEMENTECR:latest
-
-echo "The following values will need to be plugged into the Tenant Management Kubernetes manifest before deployment."
-echo "**PLEASE DO NOT CLOSE THIS WINDOW**"
-echo ""
-echo 'TENANT_MANGEMENT_ECR_REPO:' $TENANTMANAGEMENTECR
-cd $CWD
-
-envsubst < ./services/shared/apps/tenant-management/k8s/partial-template.txt > ./services/shared/apps/tenant-management/k8s/template.yaml
