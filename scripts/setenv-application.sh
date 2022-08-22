a470ffcf07624044c0a4bc229c69860381ac19a9
Ranjith Raman <ranraman@amazon.com>
2022-08-22 10:32:29 -0500

    bug fix setenv app services missing export stacks

--- scripts/setenv-application.sh
+++ scripts/setenv-application.sh
@@ -2,5 +2,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+export STACKS=$(aws cloudformation describe-stacks)
 export REGION=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AWSRegion") | .OutputValue')
 export ELBURL=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="ELBURL") | .OutputValue')
