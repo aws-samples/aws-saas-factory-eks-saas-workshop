09b041db15fb12cb4332ac66b81a0c17bbf3e1a6
Ranjith Raman <ranraman@amazon.com>
2022-08-25 12:38:41 -0500

    deploy product script fixes

--- scripts/deploy-product.sh
+++ scripts/deploy-product.sh
@@ -10,11 +10,11 @@ export ELBURL=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=
 export PRODUCTSERVICEECR=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="ProductServiceECR") | .OutputValue')
 
 CWD=$(pwd)
-cd ./services/application
+cd ./services
 REGISTRY=$(echo $PRODUCTSERVICEECR| cut -d'/' -f 1)
 TENANTREGCONTAINERNAME=$(echo $PRODUCTSERVICEECR| cut -d'/' -f 2)
 
-# Logs into our private ECR so we can push this image
+#Logs into our private ECR so we can push this image
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REGISTRY
 # Logs into public ECR so we can pull the images referenced in this dockerfile
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
@@ -22,6 +22,6 @@ docker build -t product-svc:latest -f Dockerfile.product .
 docker tag product-svc:latest $PRODUCTSERVICEECR:latest
 docker push $PRODUCTSERVICEECR:latest
 
-TENANTPATH=app CONTAINERIMAGE=$PRODUCTSERVICEECR:latest envsubst < ./services/apps/application/product/k8s/template.txt > ./services/apps/application/product/k8s/template.yaml
-kubectl apply -f ./services/apps/application/product/k8s/template.yaml
+TENANTPATH=app CONTAINERIMAGE=$PRODUCTSERVICEECR:latest envsubst < ./apps/application/product/k8s/template.txt > ./apps/application/product/k8s/template.yaml
+kubectl apply -f ./apps/application/product/k8s/template.yaml
 
