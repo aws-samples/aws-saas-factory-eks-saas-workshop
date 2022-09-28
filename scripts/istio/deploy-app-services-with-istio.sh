da94826114d5d71c9a2e257afc169778bc211b25
Ranjith Raman <ranraman@amazon.com>
2022-09-28 12:13:59 -0500

    Lab 4 and 5 updates - istio install and traffic routing changes

--- scripts/istio/deploy-app-services-with-istio.sh
+++ scripts/istio/deploy-app-services-with-istio.sh
@@ -11,5 +11,5 @@ TENANTPATH=lab5 CONTAINERIMAGE=public.ecr.aws/o2b5n0j5/eks-saas-product:latest e
 kubectl apply -f ./services/apps/application/product/k8s/template.yaml
 
 source ./scripts/istio/setenv-order-with-istio.sh
-TENANTPATH=lab5 CONTAINERIMAGE=public.ecr.aws/o2b5n0j5/eks-saas-order:latest envsubst < ./services/apps/application/order/k8s/template.istio.txt > ./services/apps/application/order/k8s/template.yaml
+TENANTPATH=lab5 CONTAINERIMAGE=public.ecr.aws/o2b5n0j5/eks-saas-order:latest CONTAINERIMAGEV2=public.ecr.aws/o2b5n0j5/eks-saas-order:v2 envsubst < ./services/apps/application/order/k8s/template.istio.txt > ./services/apps/application/order/k8s/template.yaml
 kubectl apply -f ./services/apps/application/order/k8s/template.yaml
