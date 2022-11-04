f3789eccb3bc380ea06b8eac6aea0c0c7606876c
Toby Buckley <74737385+tobuck-aws@users.noreply.github.com>
2022-11-04 11:58:31 -0700

    Fix workshop zip file (#55)

--- workshop-stack/lib/tenant-infra-stack.ts
+++ workshop-stack/lib/tenant-infra-stack.ts
@@ -81,200 +81,199 @@ export class TenantInfraStack extends NestedStack {
       pipelineName: `eks-saas-tenant-onboarding-pipeline`,
     });
 
-        // Import existing CodeCommit sam-app repository
-        const codeRepo = codecommit.Repository.fromRepositoryName(
-          this,
-          'AppRepository',
-          'aws-saas-factory-eks-workshop'
-        );
-    
-        // Declare source code as an artifact
-        const sourceOutput = new codepipeline.Artifact();
-    
-        // Add source stage to pipeline
-        pipeline.addStage({
-          stageName: 'Source',
-          actions: [
-            new CodeCommitSourceAction({
-              actionName: 'CodeCommit_Source',
-              repository: codeRepo,
-              branch: 'main',
-              output: sourceOutput,
-              variablesNamespace: 'SourceVariables',
-            }),
-          ],
-        });
-    
-        // Declare build output as artifacts
-        new codepipeline.Artifact();
-    
-        // Add the Lambda invoke stage to our pipeline
-        const pipelinePolicy = new iam.PolicyDocument({
-          assignSids: false,
-          statements: [
-            new iam.PolicyStatement({
-              effect: iam.Effect.ALLOW,
-              actions: ['codepipeline:PutJobSuccessResult', 'codepipeline:PutJobFailureResult'],
-              resources: ['*'],
-            }),
-          ],
-        });
-    
-        const lambdaRole = new iam.Role(this, 'EksTenantStackLambdaRole', {
-          assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-          inlinePolicies: {
-            inlinePolicy: pipelinePolicy,
-          },
-        });
-    
-        this.pipelineFunction = new lambda.Function(this, 'Func', {
-          runtime: lambda.Runtime.NODEJS_12_X,
-          handler: 'index.handler',
-          code: lambda.Code.fromInline(`
-          var assert = require('assert');
-          var AWS = require('aws-sdk');
-          var http = require('http');
-          var codepipeline = new AWS.CodePipeline();
-    
-          exports.handler = async (event, context) => {
-            var outputParams;
-            var tenantName,appClientId,userPoolId,elbUrl,codeBuildArn,iamRoleArn;
-            var region = process.env.AWS_REGION;
-            
-            console.log('event:', event);
-            
-            // Retrieve the Job ID from the Lambda action
-            var jobId = event["CodePipeline.job"].id;
-    
-            // Retrieve the tenant data from Tenant-Stack-Mapping table
-            const result = await getTenantStackData();
-            console.log("result====>", result);
-    
-              result.Items.forEach(function (element, index, array) {
-                tenantName = element.TenantName.S;
-                appClientId = element.AppClientId.S;
-                userPoolId = element.UserPoolId.S;
-              });
-      
-              console.log("TenantName====>", tenantName);
-              console.log("appClientId====>", appClientId);
-              console.log("userPoolId====>", userPoolId);
-              console.log("region====>", region);
-            
-            // Retrieve the stack data from the EKS-SaaS-Stack-Metadata table
-            const stackMetadata = await getStackMetadata();
-            console.log("stackMetadata====>", stackMetadata);
-              stackMetadata.Items.forEach(function (element, index, array) {
-                elbUrl = element.ELBURL.S;
-                codeBuildArn = element.CODEBUILD_ARN.S;
-                iamRoleArn = element.IAM_ROLE_ARN.S;
-              });
-              
-              console.log("ELBURL====>", elbUrl);
-              console.log("CODEBUILD_ARN====>", codeBuildArn);
-              console.log("IAM_ROLE_ARN====>", iamRoleArn);
-      
-    
-            outputParams = {
-                jobId: jobId,
-                outputVariables: {
-                    TENANT_PATH: tenantName,
-                    COGNITO_USER_POOL_ID: userPoolId,
-                    COGNITO_CLIENT_ID: appClientId,
-                    ELBURL: elbUrl,
-                    CODEBUILDARN: codeBuildArn,
-                    IAM_ROLE_ARN: iamRoleArn,
-                    Region: region,
-                    dateTime: Date(Date.now()).toString(),
-                }
-            };
-            
-            // Notify AWS CodePipeline of a successful job
-            await putJobSuccess(outputParams);
-          }
-     
-          async function putJobSuccess(params) {
-             return codepipeline.putJobSuccessResult(params).promise();
-            };
-    
-          async function getTenantStackData() {
-            //Query DynamoDB table for tenant stack data
-            var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-    
-            var params = {
-              ExpressionAttributeValues: {
-                ':s': {S: 'Provisioning'},
-              },
-              FilterExpression: 'DeploymentStatus = :s',
-              TableName: 'EKS-SaaS-Tenant-Stack-Mapping',
-    
-            };
-            
-            return ddb.scan(params).promise();
-          }
+    // Import existing CodeCommit sam-app repository
+    const codeRepo = codecommit.Repository.fromRepositoryName(
+      this,
+      'AppRepository',
+      'aws-saas-factory-eks-workshop'
+    );
+
+    // Declare source code as an artifact
+    const sourceOutput = new codepipeline.Artifact();
+
+    // Add source stage to pipeline
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new CodeCommitSourceAction({
+          actionName: 'CodeCommit_Source',
+          repository: codeRepo,
+          branch: 'main',
+          output: sourceOutput,
+          variablesNamespace: 'SourceVariables',
+        }),
+      ],
+    });
+
+    // Declare build output as artifacts
+    new codepipeline.Artifact();
+
+    // Add the Lambda invoke stage to our pipeline
+    const pipelinePolicy = new iam.PolicyDocument({
+      assignSids: false,
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ['codepipeline:PutJobSuccessResult', 'codepipeline:PutJobFailureResult'],
+          resources: ['*'],
+        }),
+      ],
+    });
+
+    const lambdaRole = new iam.Role(this, 'EksTenantStackLambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        inlinePolicy: pipelinePolicy,
+      },
+    });
+
+    this.pipelineFunction = new lambda.Function(this, 'Func', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+      var assert = require('assert');
+      var AWS = require('aws-sdk');
+      var http = require('http');
+      var codepipeline = new AWS.CodePipeline();
+
+      exports.handler = async (event, context) => {
+        var outputParams;
+        var tenantName,appClientId,userPoolId,elbUrl,codeBuildArn,iamRoleArn;
+        var region = process.env.AWS_REGION;
+        
+        console.log('event:', event);
+        
+        // Retrieve the Job ID from the Lambda action
+        var jobId = event["CodePipeline.job"].id;
+
+        // Retrieve the tenant data from Tenant-Stack-Mapping table
+        const result = await getTenantStackData();
+        console.log("result:", result);
+
+          result.Items.forEach(function (element, index, array) {
+            tenantName = element.TenantName.S;
+            appClientId = element.AppClientId.S;
+            userPoolId = element.UserPoolId.S;
+          });
+  
+          console.log("TenantName:", tenantName);
+          console.log("appClientId:", appClientId);
+          console.log("userPoolId:", userPoolId);
+          console.log("region:", region);
+        
+        // Retrieve the stack data from the EKS-SaaS-Stack-Metadata table
+        const stackMetadata = await getStackMetadata();
+        console.log("stackMetadata:", stackMetadata);
+          stackMetadata.Items.forEach(function (element, index, array) {
+            elbUrl = element.ELBURL.S;
+            codeBuildArn = element.CODEBUILD_ARN.S;
+            iamRoleArn = element.IAM_ROLE_ARN.S;
+          });
           
-          async function getStackMetadata() {
-            //Query DynamoDB table for EKS SaaS stack metadadata
-            var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-            var params = {
-              ExpressionAttributeValues: {
-                ':s': {S: 'eks-saas'},
-              },
-              KeyConditionExpression: 'StackName = :s',
-              TableName: 'EKS-SaaS-Stack-Metadata',
-            };
-            return ddb.query(params).promise();
-          }     
-          `),
-        });
-    
-        const lambdaInvokeAction = new LambdaInvokeAction({
-          actionName: 'Lambda',
-          lambda: this.pipelineFunction,
-          variablesNamespace: 'LambdaVariables',
-        });
-    
-        pipeline.addStage({
-          stageName: 'Lambda',
-          actions: [lambdaInvokeAction],
-        });
-    
-        // Declare build output as artifacts
-        const buildOutput = new codepipeline.Artifact();
-    
-        //Declare a new CodeBuild project
-    
-        const codeBuildRole = getCodeBuildRole(this, this.account, this.region);
-    
-        const buildProject = new codebuild.PipelineProject(this, 'Build', {
-          buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yaml'),
-          role: codeBuildRole,
-          environment: {
-            buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
-            privileged: true,
+          console.log("ELBURL:", elbUrl);
+          console.log("CODEBUILD_ARN:", codeBuildArn);
+          console.log("IAM_ROLE_ARN:", iamRoleArn);
+  
+
+        outputParams = {
+            jobId: jobId,
+            outputVariables: {
+                TENANT_PATH: tenantName,
+                USERPOOLID: userPoolId,
+                APPCLIENTID: appClientId,
+                ELBURL: elbUrl,
+                CB_ARN: codeBuildArn,
+                IAM_ARN: iamRoleArn,
+                REGION: region,
+                DATETIME: Date(Date.now()).toString(),
+            }
+        };
+        
+        // Notify AWS CodePipeline of a successful job
+        await putJobSuccess(outputParams);
+      }
+ 
+      async function putJobSuccess(params) {
+         return codepipeline.putJobSuccessResult(params).promise();
+        };
+
+      async function getTenantStackData() {
+        //Query DynamoDB table for tenant stack data
+        var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+        var params = {
+          ExpressionAttributeValues: {
+            ':s': {S: 'Provisioning'},
           },
-        });
-    
-        const codeBuildAction = new CodeBuildAction({
-          actionName: 'Build-And-Deploy-Tenant-K8s-resources',
-          project: buildProject,
-          input: sourceOutput,
-          outputs: [buildOutput],
-          environmentVariables: {
-            TENANT_PATH: { value: lambdaInvokeAction.variable('TENANT_PATH') },
-            COGNITO_USER_POOL_ID: { value: lambdaInvokeAction.variable('COGNITO_USER_POOL_ID') },
-            COGNITO_CLIENT_ID: { value: lambdaInvokeAction.variable('COGNITO_CLIENT_ID') },
-            ELBURL: { value: lambdaInvokeAction.variable('ELBURL') },
-            CODEBUILDARN: { value: lambdaInvokeAction.variable('CODEBUILDARN') },
-            IAM_ROLE_ARN: { value: lambdaInvokeAction.variable('IAM_ROLE_ARN') },
+          FilterExpression: 'DeploymentStatus = :s',
+          TableName: 'EKS-SaaS-Tenant-Stack-Mapping',
+
+        };
+        
+        return ddb.scan(params).promise();
+      }
+      
+      async function getStackMetadata() {
+        //Query DynamoDB table for EKS SaaS stack metadadata
+        var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+        var params = {
+          ExpressionAttributeValues: {
+            ':s': {S: 'eks-saas'},
           },
-        });
-    
-        // Add the build stage to our pipeline
-        pipeline.addStage({
-          stageName: 'Build',
-          actions: [codeBuildAction],
-        });
-    
+          KeyConditionExpression: 'StackName = :s',
+          TableName: 'EKS-SaaS-Stack-Metadata',
+        };
+        return ddb.query(params).promise();
+      }     
+      `),
+    });
+
+    const lambdaInvokeAction = new LambdaInvokeAction({
+      actionName: 'Lambda',
+      lambda: this.pipelineFunction,
+      variablesNamespace: 'LambdaVariables',
+    });
+
+    pipeline.addStage({
+      stageName: 'Lambda',
+      actions: [lambdaInvokeAction],
+    });
+
+    // Declare build output as artifacts
+    const buildOutput = new codepipeline.Artifact();
+
+    //Declare a new CodeBuild project
+
+    const codeBuildRole = getCodeBuildRole(this, this.account, this.region);
+
+    const buildProject = new codebuild.PipelineProject(this, 'Build', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yaml'),
+      role: codeBuildRole,
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+        privileged: true,
+      },
+    });
+
+    const codeBuildAction = new CodeBuildAction({
+      actionName: 'Build-And-Deploy-Tenant-K8s-resources',
+      project: buildProject,
+      input: sourceOutput,
+      outputs: [buildOutput],
+      environmentVariables: {
+        TENANT_PATH: { value: lambdaInvokeAction.variable('TENANT_PATH') },
+        USERPOOLID: { value: lambdaInvokeAction.variable('USERPOOLID') },
+        APPCLIENTID: { value: lambdaInvokeAction.variable('APPCLIENTID') },
+        ELBURL: { value: lambdaInvokeAction.variable('ELBURL') },
+        CB_ARN: { value: lambdaInvokeAction.variable('CB_ARN') },
+        IAM_ARN: { value: lambdaInvokeAction.variable('IAM_ARN') },
+      },
+    });
+
+    // Add the build stage to our pipeline
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [codeBuildAction],
+    });
   }
 }
