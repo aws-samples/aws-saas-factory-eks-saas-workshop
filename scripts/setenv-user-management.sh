a32ac26fef4180f1ab49ea729d23c3695e99b74d
Ranjith Raman <ranraman@amazon.com>
2022-11-15 08:09:39 -0600

    Making setenv-user-management consistent with other setenv scripts

--- scripts/setenv-user-management.sh
+++ scripts/setenv-user-management.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: MIT-0
 
 if [ -z "$STACKS" ]; then export STACKS=$(aws cloudformation describe-stacks);fi
-export REGION=$(echo $STACKS | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="AWSRegion") | .OutputValue' 2> /dev/null)
-export USERPOOLID=$(echo $STACKS | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="AdminUserPoolId") | .OutputValue' 2> /dev/null)
-export APPCLIENTID=$(echo $STACKS | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="AdminAppClientId") | .OutputValue' 2> /dev/null)
+export REGION=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AWSRegion") | .OutputValue')
+export USERPOOLID=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AdminUserPoolId") | .OutputValue')
+export APPCLIENTID=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AdminAppClientId") | .OutputValue')
