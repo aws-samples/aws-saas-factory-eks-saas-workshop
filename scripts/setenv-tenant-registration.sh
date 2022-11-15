7efd247d06bd27a6737ea3cf0a44f892d39cb145
Toby Buckley <tobuck@amazon.com>
2022-11-15 10:06:28 -0800

    now unconditionally exports CFN outputs

--- scripts/setenv-tenant-registration.sh
+++ scripts/setenv-tenant-registration.sh
@@ -2,7 +2,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-if [ -z "$STACKS" ]; then export STACKS=$(aws cloudformation describe-stacks);fi
+export STACKS=$(aws cloudformation describe-stacks)
 export REGION=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AWSRegion") | .OutputValue')
 export USERPOOLID=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AdminUserPoolId") | .OutputValue')
 export APPCLIENTID=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="AdminAppClientId") | .OutputValue')
