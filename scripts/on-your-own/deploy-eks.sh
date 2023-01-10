10d42c467420cb343ce89bf42a130f8f781e6433
Toby Buckley <tobuck@amazon.com>
2023-01-09 16:32:02 -0800

    changing cluster name to correspond with buildspec.yml

--- scripts/on-your-own/deploy-eks.sh
+++ scripts/on-your-own/deploy-eks.sh
@@ -9,9 +9,9 @@ yarn && yarn run build
 cdk bootstrap  
 cdk deploy 
 
-export ELBURL=$(aws cloudformation describe-stacks --stack-name EksStack --query "Stacks[0].Outputs[?OutputKey=='ELBURL'].OutputValue" --output text)
-export CODEBUILD_ARN=$(aws cloudformation describe-stacks --stack-name EksStack --query "Stacks[0].Outputs[?OutputKey=='EksCodebuildArn'].OutputValue" --output text)
-export IAM_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name EksStack --query "Stacks[0].Outputs[?OutputKey=='RoleUsedByTVM'].OutputValue" --output text)
+export ELBURL=$(aws cloudformation describe-stacks --stack-name ClusterStack --query "Stacks[0].Outputs[?OutputKey=='ELBURL'].OutputValue" --output text)
+export CODEBUILD_ARN=$(aws cloudformation describe-stacks --stack-name ClusterStack --query "Stacks[0].Outputs[?OutputKey=='EksCodebuildArn'].OutputValue" --output text)
+export IAM_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name ClusterStack --query "Stacks[0].Outputs[?OutputKey=='RoleUsedByTVM'].OutputValue" --output text)
 
 echo "export ELBURL=${ELBURL}" | tee -a ~/.bash_profile
 echo "export IAM_ROLE_ARN=${IAM_ROLE_ARN}" | tee -a ~/.bash_profile
